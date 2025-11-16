d10b0adb46d562fccc1ebf2b31b6bea961d910c8
amala <amalananda332@gmail.com>
2025-11-16 17:07:05 +0700

    feat: update image configuration to use remote patterns for better image handling

--- ulaman-clone/next.config.ts
+++ ulaman-clone/next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'images.prismic.io', 'images.remotePatterns'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
